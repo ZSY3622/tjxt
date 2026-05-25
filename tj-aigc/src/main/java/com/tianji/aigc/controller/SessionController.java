839cdd0eab7b7aa1f922d59ffd65cb95a2a6431a
zsy <1179611284@qq.com>
2026-05-25 22:39:32 +0800

    修改会话标题、删除会话、查询会话

--- tj-aigc/src/main/java/com/tianji/aigc/controller/SessionController.java
+++ tj-aigc/src/main/java/com/tianji/aigc/controller/SessionController.java
@@ -1,12 +1,14 @@
 package com.tianji.aigc.controller;
 
 import com.tianji.aigc.service.ChatSessionService;
+import com.tianji.aigc.vo.ChatSessionVO;
 import com.tianji.aigc.vo.MessageVO;
 import com.tianji.aigc.vo.SessionVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/session")
@@ -30,9 +32,30 @@ public class SessionController {
         return this.chatSessionService.hotExamples(num);
     }
 
+    /**
+     *
+     * @param sessionId
+     * @return
+     */
     @GetMapping("/{sessionId}")
     public List<MessageVO> queryBySessionId(@PathVariable("sessionId") String sessionId) {
         return this.chatSessionService.queryBySessionId(sessionId);
+    }
+
+    @GetMapping("/history")
+    public Map<String,List<ChatSessionVO>> queryHistorySession(){
+        return this.chatSessionService.queryHistorySession();
+    }
+
+    @DeleteMapping("/history")
+    public void deleteHistorySession(@RequestParam("sessionId") String sessionId) {
+        this.chatSessionService.deleteHistorySession(sessionId);
+    }
+
+    @PutMapping("/history")
+    public void updateHistorySessionTitle(@RequestParam("sessionId") String sessionId,
+                                          @RequestParam("title") String title) {
+        this.chatSessionService.updateHistorySessionTitle(sessionId, title);
     }
 
 }
