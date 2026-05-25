f8cf21f70704721383dcbba819cb14537772650d
zsy <1179611284@qq.com>
2026-05-25 20:54:05 +0800

    RAG实现

--- tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
+++ tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
@@ -15,9 +15,12 @@ import com.tianji.common.utils.UserContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -36,8 +39,10 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 @RequiredArgsConstructor
 public class ChatServiceImpl implements ChatService {
-//    @Qualifier("chatClient1") 用于指定bean
+    //    @Qualifier("chatClient1") 用于指定bean
     private final ChatMemory chatMemory;
+
+    private final VectorStore vectorStore;
 
     private final ChatClient chatClient;
     private final SystemPromptConfig systemPromptConfig;
@@ -59,22 +64,33 @@ public class ChatServiceImpl implements ChatService {
 
         Long userId = UserContext.getUser();
 
+        //创建RA增强
+        QuestionAnswerAdvisor questionAnswerAdvisor = QuestionAnswerAdvisor.builder(vectorStore)
+                .searchRequest(
+                        SearchRequest.builder().similarityThreshold(0.6d) //相似度阈值
+                                .topK(6) //最多返回6条文档
+                                .build())
+                .build();
+
+
         return this.chatClient.prompt()
                 .system(promptSystem -> promptSystem.text(systemPromptConfig.getChatSystemMessage().get()) //设置提示词
                         .param("now", DateUtil.now()) //设置提示词中的时间参数
                 )
-                .advisors(advisor -> advisor.param(ChatMemory.CONVERSATION_ID,conversationId))// 生成对话id的逻辑
-                .toolContext(Map.of(Constant.REQUEST_ID,requestId,Constant.USER_ID,userId)) // 通过工具上下文传递工具
+                .advisors(advisor ->
+                        advisor.advisors(questionAnswerAdvisor) //设置RAG增强
+                                .param(ChatMemory.CONVERSATION_ID, conversationId))// 生成对话id的逻辑
+                .toolContext(Map.of(Constant.REQUEST_ID, requestId, Constant.USER_ID, userId)) // 通过工具上下文传递工具
                 .user(question)
                 .stream()
                 .chatResponse()
-                .doFirst(()->GENERATE_STATUS.put(sessionId,true))//第一次生成时候执行
+                .doFirst(() -> GENERATE_STATUS.put(sessionId, true))//第一次生成时候执行
                 .doOnError(throwable -> GENERATE_STATUS.remove(sessionId)) // 出现异常时，删除标识
                 .doOnComplete(() -> GENERATE_STATUS.remove(sessionId)) // 完成时执行，删除标识
-                .doOnCancel(()->{
-                    saveStopHistoryRecord(conversationId,outputBuilder.toString());
+                .doOnCancel(() -> {
+                    saveStopHistoryRecord(conversationId, outputBuilder.toString());
                 })//中断输出,执行
-                .takeWhile(chatResponse -> GENERATE_STATUS.getOrDefault(sessionId,false)) //根据sessionIdd 状态来判断是否停止生成
+                .takeWhile(chatResponse -> GENERATE_STATUS.getOrDefault(sessionId, false)) //根据sessionIdd 状态来判断是否停止生成
                 .map(chatResponse -> {
                     // 对于响应结果进行处理，如果是最后一条数据，就把此次消息id放到内存中 ,因为工具也是最后输出给前端的
                     String finishReason = chatResponse.getResult().getMetadata().getFinishReason();
@@ -92,14 +108,14 @@ public class ChatServiceImpl implements ChatService {
                             .eventType(ChatEventTypeEnum.DATA.getValue())
                             .build();
                 })
-                .concatWith(Flux.defer(()->{
+                .concatWith(Flux.defer(() -> {
                     // 通过请求id获取到参数列表，如果不为空，就将其追加到返回结果中
                     Map<String, Object> map = ToolResultHolder.get(requestId);
-                    if (CollUtil.isNotEmpty(map)){
+                    if (CollUtil.isNotEmpty(map)) {
                         ToolResultHolder.remove(requestId); // 清除参数列表
                         // 将通过tools得到对应的数据返回给前端
                         ChatEventVO chatEventVO = ChatEventVO.builder().eventData(map).eventType(ChatEventTypeEnum.PARAM.getValue()).build();
-                        return Flux.just(chatEventVO,STOP_EVENT);
+                        return Flux.just(chatEventVO, STOP_EVENT);
                     }
                     return Flux.just(STOP_EVENT);
                 }));
